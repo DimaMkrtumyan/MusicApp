8a67f9eb0360d350464e5c2f8bfb03215a447dcd
DimaMkrtumyan <dmitriy.mkrtumyan@gmail.com>
2023-01-19 13:53:13 +0400

    First commit for MusicApp

---
+++ Music App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music App
+//
+//  Created by Dmitry Mkrtumyan on 19.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
